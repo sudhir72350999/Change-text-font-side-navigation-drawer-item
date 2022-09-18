2b0ac9008a6ae7dd0219ef07bea1130d7c30bb57
sudhir <sudhirpraj0603@gmail.com>
2022-09-18 05:45:50 +0530

    Change-text-font-side-navigation-drawer-item

---
+++ app/src/main/java/com/sudhirtheindian/changethetextfontofsidenavigationdraweritemssp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sudhirtheindian.changethetextfontofsidenavigationdraweritemssp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
